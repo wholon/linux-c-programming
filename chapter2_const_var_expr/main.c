e1fa3ddbea4561e320b6d915c7b9f543aaee61f2
Holon <6367412@qq.com>
2020-12-06 16:11:12 +0800

    delete old dir structure

--- chapter2_const_var_expr/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/* main: generate some simple output */
-int main(void)
-{
-	printf("Hello, World!\n");
-	printf("\a");
-	return 0;
-}
--- chapter3_simpleFunction/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/* a main function with return not zero */
-
-int main(void)
-{
-	return 4;
-}
